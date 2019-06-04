bcc2429080d27be3878e943fa9caf84d10fecb74
moodymillennial <51368281+moodymillennial@users.noreply.github.com>
2019-06-04 14:41:20 +0200

    Create hello-world.cpp

---
+++ hello-world.cpp
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////////////////////////////
+// Author: Joshua Stromberg
+// Program Description: Output the phrase "Hello World, my name is ****"
+//  Input: User executes program
+//  Processing: Runs the code to display the message
+//  Output: Displays "Hello World, my name is Joshua Stromberg"
+//////////////////////////////////////////////////////////////////////////////
+ 
+#include <iostream>                            // for I/O
+using namespace std;
+ 
+// main program
+ 
+// -----------------------------------------------------------------------------------
+// Description: main() displays the message
+//   input: user runs program
+//   returns: message "Hello World, my name is Joshua Stromberg"
+//------------------------------------------------------------------------------------
+int main()
+{
+    cout << "Hello World, my name is Joshua Stromberg";
+    return 0;
+ 
+}
